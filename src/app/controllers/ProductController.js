52ebddc103c4f130ce1dd7954e656dceba671b1c
Aloisio Martinez <aloisiojacobs36@gmail.com>
2020-01-29 23:25:48 -0300

    Add files via upload

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -3,7 +3,7 @@ const Product = require('../models/Product')
 const File = require('../models/File')
 
 
-const { formatPrice } = require('../../lib/utils')
+const { formatPrice, date } = require('../../lib/utils')
 
 module.exports = {
   create(req, res) {
@@ -41,6 +41,26 @@ module.exports = {
 
       return res.redirect(`/products/${productId}/edit`)
 
+  },
+  async show (req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if (!product) return res.send("Product Not Found")
+
+    const { day, hour, minutes, month} = date(product.updated_at)
+
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}h${minutes}`,
+      
+    }
+
+    product.oldPrice = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+    
+    return res.render("products/show", { product })
   },
   async edit(req, res) {
     let results = await Product.find(req.params.id)
@@ -104,7 +124,7 @@ module.exports = {
 
     await Product.update(req.body)
 
-    return res.redirect(`/products/${req.body.id}/edit`)
+    return res.redirect(`/products/${req.body.id}`)
   },
   async delete(req, res) {
     await Product.delete(req.body.id)
